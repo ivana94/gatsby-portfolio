d5da57e2663389bfb075d263d5ae17cc8c25f757
Ivana Matijevic <ivmatij@gmail.com>
2021-11-21 01:34:53 -0500

    removes unused code

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { graphql, Link } from "gatsby";
+import { graphql } from "gatsby";
 import { Layout } from "../components/Layout";
 import { serializers } from "../components/serializers";
 import { PortableText } from "../components/PortableText";
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Layout } from "../components/Layout";
-import styled, { keyframes, css } from "styled-components";
+import styled, { css } from "styled-components";
 
 const generateConfettiExplosionAnimation = () => {
     let styles = "";
